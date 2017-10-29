bc3bfe799fdd9025fce5dfc62d42e71ba46226f6
Krasnodaretc <egorzhivagin@gmail.com>
2017-10-29 21:50:09 +0300

    watcher

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,9 @@ var gulp = require('gulp'),
     concat = require("gulp-concat");
     autoprefixer = require('gulp-autoprefixer');
 
-gulp.task("default", ["sass", "js"]);
+gulp.task("default", ["sass-dev", "js", "html", "png"]);
 
-gulp.task("sass", function () {
+gulp.task("sass-dev", function () {
    gulp.src("./style/**/*.scss")
        .pipe(sass().on('error', sass.logError))
        .pipe(concat("main.css"))
@@ -25,4 +25,22 @@ gulp.task("js", function () {
        .pipe(concat("main.js"))
        .pipe(babel())
        .pipe(gulp.dest("./build/js"))
+});
+
+
+gulp.task("html", function () {
+    gulp.src("./views/*.html")
+        .pipe(gulp.dest("./build/"))
+});
+
+
+gulp.task("png", function () {
+   gulp.src("./public/img/**/*.png")
+       .pipe(gulp.dest("./build/img/"))
+});
+
+gulp.task('watch', function () {
+    gulp.watch("./style/**/*.scss", ['sass-dev']);
+    gulp.watch("./public/js/**/*.js", ['js']);
+    gulp.watch("./views/*.html", ['html']);
 });
